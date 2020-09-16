dc7374de2f6f05dcf9e22978ff146f8aef1b1fcf
Stian Toften <stiantoften@gmail.com>
2020-09-16 17:20:11 +0200

    Fix for redirecting urls to correct page

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -55,10 +55,10 @@
 			<div class="card" in:fly={{y: 20}}>
 				<h2>Om Beta</h2>
 				<p>
-					Beta er linjeforeningen for Dataingeniør Studenter ved
-					Universitetet i Agder. Beta er en non-profit organisasjon.
-					Det betyr Beta ikke driver for å tjene penger, men heller
-					bruker fortjenesten på våre medlemmer. Alle studenter ved
+					Beta er linjeforeningen for datastudenter ved Universitetet
+					i Agder. Beta er en non-profit organisasjon. Det betyr Beta
+					ikke driver for å tjene penger, men heller bruker
+					fortjenesten på våre medlemmer. Alle studenter ved
 					datarelaterte fag ved UiA er automatisk medlem av Beta
 					dersom en ikke aktivt melder seg ut. Det betyr at vi idag
 					har omlag 500 medlemmer.
@@ -69,21 +69,21 @@
 				<h2>Historie</h2>
 				<p>
 					Beta ble grunnlagt i 2006 mens det fortsatt het Høyskolen i
-					Agder, deretter, under flyttingen i 2010 så fulgte Beta med
-					inn på Universitetet i Agder.
+					Agder, deretter, under flyttingen i 2010 fulgte Beta med inn
+					på Universitetet i Agder.
 				</p>
 				<p>
 					Beta har hatt en lang tradisjon med å forbedre det
-					studentsosiale mulighet rundt studiet for studenter. Beta
-					har blant annet arrangert BetaLAN og Retrokveld som er helt
-					åpent for alle studenter, og arrangert bedriftpresentasjoner
-					som har hjulpet studenter ut i arbeidslivet.
+					studentsosiale mulighet rundt studiet for studenter. Vi
+					arrangerer blant annet BetaLAN og Retrokveld som er helt
+					åpent for alle studenter, samt bedriftpresentasjoner som har
+					hjulpet studenter ut i arbeidslivet.
 				</p>
 				<p>
 					Siden oppstarten har Beta vært underlagt STA
 					(STudentorganisasjonen i Agder), men det var først i 2014 at
 					Beta for første gang ble registrert i Brønnøysund Registeret
-					som en frivillig organisasjon under STA..
+					som en frivillig organisasjon under STA.
 				</p>
 			</div>
 			<div class="card" in:fly={{y: 20}}>
